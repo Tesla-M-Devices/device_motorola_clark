1be628d20762a25d735e1127e78a68f1dc770168
slebit <tillnagotannat@gmail.com>
2016-05-26 18:07:58 +0200

    Build Validus
    Change-Id: Ia81d0be20718939ea4e29be121f762ea01df1098

--- cm.mk
+++ cm.mk
@@ -2,20 +2,35 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, device/motorola/clark/device.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/validus/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/validus/configs/telephony.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/configs/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/clark/device.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_clark
+PRODUCT_NAME := validus_clark
 PRODUCT_BRAND := Android
 PRODUCT_DEVICE := clark
 PRODUCT_MODEL := XT1575
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_PROPERTY_OVERRIDES +=\
+    ro.product.device=clark
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="Moto X Pure"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
